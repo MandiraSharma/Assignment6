6abbdce7f82af3e3f9b0e82ce00460adf8558350
mandiosho <60794614+mandiosho@users.noreply.github.com>
2020-05-18 16:57:57 -0500

    commit1

---
+++ src/test/java/test/BankAndCashTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import pages.BankandCashPage;
+import pages.LoginPage;
+import util.BrowserFactory;
+
+public class BankAndCashTest {
+	
+	
+	
+	WebDriver driver;
+
+	@Test
+	public void validusershouldbeabletologin() throws InterruptedException {
+		// connect with BrowserFactory
+		driver = BrowserFactory.launchBrowser();
+
+		// create page factory to call the LoginPage
+		LoginPage LoginPage = PageFactory.initElements(driver, LoginPage.class);
+		BankandCashPage BankandCashPage =PageFactory.initElements(driver, BankandCashPage.class);
+		// call the methods
+		LoginPage.enteredUserName("techfiosdemo@gmail.com");
+		LoginPage.enteredPassword("abc123");
+		LoginPage.clickSigninButton();
+		
+		//Thread.sleep(2000);
+		BankandCashPage.clickBankAndCash();
+		//Thread.sleep(2000);
+		BankandCashPage.clickNewAC();
+		//Thread.sleep(2000);
+		BankandCashPage.enterACtitle("Aurther");
+		BankandCashPage.enterDescription("Backyard Project");
+		BankandCashPage.enterNewBalance("500");
+		BankandCashPage.clickSubmitButton();
+		BankandCashPage.veryfyACcreatedMSG();
+
+	}
+
+}
